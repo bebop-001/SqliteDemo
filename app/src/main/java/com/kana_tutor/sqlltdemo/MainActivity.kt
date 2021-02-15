1295a842b413e95227cdbf3d71df750231e1284a
sjs <sjs@sonic.net>
2021-02-14 16:05:21 -0800

    delete works.  Also hiding keyboard, enable/disable keys, etc.

--- app/src/main/java/com/kana_tutor/sqlltdemo/MainActivity.kt
+++ app/src/main/java/com/kana_tutor/sqlltdemo/MainActivity.kt
@@ -5,29 +5,33 @@ package com.kana_tutor.sqlltdemo
 // goal is to setup sqllite db, query, add, and
 // delete items.
 
+import android.app.Activity
 import android.content.ContentValues
 import android.content.Context
 import android.database.sqlite.SQLiteDatabase
 import android.database.sqlite.SQLiteOpenHelper
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.view.inputmethod.InputMethodManager
 import android.widget.ArrayAdapter
-import android.widget.EditText
 import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.widget.SwitchCompat
+import androidx.core.widget.doAfterTextChanged
 import com.kana_tutor.sqlltdemo.databinding.ActivityMainBinding
 
-data class CustomerModel (
-    val id:Int,
-    val name:String,
-    val age:Int,
-    val isActive:Boolean,
-        )
-class DbHelper(context: Context,
-               name:String,
-               factory: SQLiteDatabase.CursorFactory?,
-               version:Int
+
+data class CustomerModel(
+    val id: Int,
+    val name: String,
+    val age: Int,
+    val isActive: Boolean,
+)
+class DbHelper(
+    context: Context,
+    name: String,
+    factory: SQLiteDatabase.CursorFactory?,
+    version: Int
 ) : SQLiteOpenHelper(context, name, factory, version) {
     val CUST_TABLE = "CUSTOMER_TABLE"
     val COL_NAME = "COLUMN_CUSTOMER_NAME"
@@ -75,7 +79,7 @@ class DbHelper(context: Context,
 
     fun addCustomer(customerModel: CustomerModel) : Boolean {
         val cv = ContentValues()
-        with (customerModel) {
+        with(customerModel) {
             cv.put(COL_NAME, name)
             cv.put(COL_AGE, age)
             cv.put(COL_ACTIVE, isActive)
@@ -86,18 +90,20 @@ class DbHelper(context: Context,
     }
     fun getAllCustomers() : MutableList<CustomerModel> {
         val queryString = """SELECT * FROM $CUST_TABLE;"""
-        val cursor = readableDatabase.rawQuery(queryString,null)
+        val cursor = readableDatabase.rawQuery(queryString, null)
         val rv = (0..(cursor.count - 1)).map{
             cursor.moveToPosition(it)
-            CustomerModel(cursor.getInt(0),
+            CustomerModel(
+                cursor.getInt(0),
                 cursor.getString(1),
                 cursor.getInt(2),
-                if (cursor.getInt(3) == 1) true else false)
+                if (cursor.getInt(3) == 1) true else false
+            )
         }.toMutableList()
         cursor.close()
         return rv
     }
-    fun getCustomer(name:String, age:Int) : CustomerModel? {
+    fun getCustomer(name: String, age: Int) : CustomerModel? {
         val queryString = """SELECT * FROM $CUST_TABLE
             |WHERE $COL_NAME = '$name' AND $COL_AGE = $age;
             |""".trimMargin()
@@ -105,12 +111,14 @@ class DbHelper(context: Context,
         val matches = mutableListOf<CustomerModel>()
         for(i in 0..cursor.count - 1) {
             cursor.moveToPosition(i)
-            matches.add(CustomerModel(
-                cursor.getInt(0),
-                cursor.getString(1),
-                cursor.getInt(2),
-                if (cursor.getInt(3) == 1) true else false
-            ))
+            matches.add(
+                CustomerModel(
+                    cursor.getInt(0),
+                    cursor.getString(1),
+                    cursor.getInt(2),
+                    if (cursor.getInt(3) == 1) true else false
+                )
+            )
         }
         cursor.close()
         if (matches.size > 1)
@@ -122,11 +130,36 @@ class DbHelper(context: Context,
             )
         return if (matches.size == 1) matches[0] else null
     }
+    fun deleteCustomer(customer: CustomerModel) =
+        deleteCustomer(customer.name, customer.age)
+    fun deleteCustomer(name: String, age: Int) : Boolean {
+        val db = this.writableDatabase
+        val queryString = """DELETE FROM $CUST_TABLE
+            |WHERE $COL_NAME = '$name' 
+            |AND $COL_AGE = $age;
+            |""".trimMargin()
+        val nRecords = db.rawQuery("SELECT * FROM $CUST_TABLE;", null).count
+        val x = db.rawQuery(queryString, null).count
+        val nRecords2 = db.rawQuery("SELECT * FROM $CUST_TABLE;", null).count
+        Log.d("DbHelper", "deleteCustomer:${nRecords - nRecords2} records deleted.")
+        db.close()
+        return nRecords != nRecords2
+    }
 
     constructor(context: Context) :
             this(context, "customer.sqlite", null, 1)
 
 }
+
+fun hideSoftKeyboard(activity: Activity) {
+    val inputMethodManager = activity.getSystemService(
+        Activity.INPUT_METHOD_SERVICE
+    ) as InputMethodManager
+    inputMethodManager.hideSoftInputFromWindow(
+        activity.currentFocus!!.windowToken, 0
+    )
+}
+
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -136,39 +169,54 @@ class MainActivity : AppCompatActivity() {
             binding.customerLv.adapter = ArrayAdapter<CustomerModel>(
                 this@MainActivity.applicationContext,
                 android.R.layout.simple_list_item_1,
-                this)
+                this
+            )
+        }
+
+        fun hideKeyboard() {
+            val im = getSystemService(INPUT_METHOD_SERVICE) as InputMethodManager
+            im.hideSoftInputFromWindow(
+                currentFocus!!.windowToken, 0
+            )
+        }
+        fun enableButtons() {
+            val name = binding.customerNameEt.text.toString()
+            val ageString = binding.customerAgeEt.text.toString()
+            binding.customerDeleteBtn.isEnabled =
+                ageString.length > 0 && name.length > 0
+                    && DbHelper(this@MainActivity.applicationContext)
+                    .getCustomer(name, ageString.toInt()) != null
+            binding.customerAddBtn.isEnabled =
+                ageString.length > 0 && name.length > 0
+                    && DbHelper(this@MainActivity.applicationContext)
+                    .getCustomer(name, ageString.toInt()) == null
         }
         run {
             val db = DbHelper(this@MainActivity.applicationContext)
             db.getAllCustomers().updateListView()
             db.close()
         }
-
         setContentView(binding.root)
-        with (binding) {
-            customerNameEt.setOnClickListener { l ->
-                customerAddBtn.isEnabled = customerNameEt.text.length > 0
-                        && customerAgeEt.text.length > 0
-                Toast.makeText(this@MainActivity.applicationContext,
-                        "customer name:" + (l as EditText).text,
-                        Toast.LENGTH_SHORT
-                ).show()
+        enableButtons()
+        with(binding) {
+            // name/age edit text to listeners so they check input on each
+            // key touch and update enable/disable for buttons.
+            customerNameEt.doAfterTextChanged { l ->
+                Log.d("xxx", "$l:\"${customerNameEt.text}\"")
+                enableButtons()
             }
-            customerAgeEt.setOnClickListener { l ->
-                customerAddBtn.isEnabled = customerNameEt.text.length > 0
-                        && customerAgeEt.text.length > 0
-                Toast.makeText(this@MainActivity.applicationContext,
-                        "customer age:" + (l as EditText).text,
-                        Toast.LENGTH_SHORT
-                ).show()
+            customerAgeEt.doAfterTextChanged { l ->
+                Log.d("xxx", "$l:\"${customerAgeEt.text}\"")
+                enableButtons()
             }
             // not really necessary since we just read state of
             // switch in customerAdd.
             customerActiveSw.setOnClickListener { l ->
                 val sw = l as SwitchCompat
-                Toast.makeText(this@MainActivity.applicationContext,
-                        "customer active:" + sw.isActivated,
-                        Toast.LENGTH_SHORT
+                Toast.makeText(
+                    this@MainActivity.applicationContext,
+                    "customer active:" + sw.isActivated,
+                    Toast.LENGTH_SHORT
                 ).show()
             }
             customerShowAllBtn.setOnClickListener {
@@ -181,11 +229,12 @@ class MainActivity : AppCompatActivity() {
                 val db = DbHelper(this@MainActivity.applicationContext)
                 val name = customerNameEt.text.toString()
                 val age = customerAgeEt.text.toString().toInt()
-                if (db.getCustomer(name,age) != null)
-                    Toast.makeText(this@MainActivity,
-                    "Customer is already in database.",
-                    Toast.LENGTH_SHORT
-                ).show()
+                if (db.getCustomer(name, age) != null)
+                    Toast.makeText(
+                        this@MainActivity,
+                        "Customer is already in database.",
+                        Toast.LENGTH_SHORT
+                    ).show()
                 else {
                     val cm = CustomerModel(-1, name, age, customerActiveSw.isChecked)
                     if (db.addCustomer(cm)) {
@@ -195,7 +244,32 @@ class MainActivity : AppCompatActivity() {
                     }
                     else Log.d("customerAdd", "Failed to add:$cm")
                 }
+                DbHelper(this@MainActivity.applicationContext)
+                    .getAllCustomers()
+                    .updateListView()
                 db.close()
+                hideKeyboard()
+            }
+            customerDeleteBtn.setOnClickListener {
+                val db = DbHelper(this@MainActivity.applicationContext)
+                if (db.deleteCustomer(
+                        customerNameEt.text.toString(),
+                        customerAgeEt.text.toString().toInt()
+                    )
+                ) {
+                    customerNameEt.setText("")
+                    customerAgeEt.setText("")
+                }
+                db.getAllCustomers().updateListView()
+                enableButtons()
+                hideKeyboard()
+            }
+            customerLv.setOnItemClickListener { parent, view, position, id ->
+                val customer = parent.getItemAtPosition(position) as CustomerModel
+                customerNameEt.setText(customer.name)
+                customerAgeEt.setText(customer.age.toString())
+                enableButtons()
+                hideKeyboard()
             }
         }
     }
